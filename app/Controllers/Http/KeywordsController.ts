a7be9e3090726810c26c1014503573d5e230bd5b
isabel9422 <isabels94@hotmail.com>
2022-06-19 18:47:22 +0200

    update (step-by-step)

--- app/Controllers/Http/KeywordsController.ts
+++ app/Controllers/Http/KeywordsController.ts
@@ -1,3 +1,17 @@
-//import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-export default class KeywordsController {}
+import Keyword from 'App/Models/Keyword'
+import CreateKeywordValidator from 'App/Validators/CreateKeywordValidator'
+
+export default class KeywordsController {
+  public async index({ response }: HttpContextContract) {
+    const keywords = await Keyword.all()
+    return response.json(keywords)
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    const validateData = await request.validate(CreateKeywordValidator)
+    const keyword = await Keyword.create(validateData)
+    return response.created({ data: keyword })
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -4,6 +4,12 @@ import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 Route.post('/login', 'UsersController.login')
 Route.post('/register', 'UsersController.store')
 
+Route.group(() => {
+  Route.get('/index/:id', 'KeywordsController.index')
+  Route.get('/index', 'KeywordsController.index')
+  Route.post('/create', 'keywordsController.store')
+}).prefix('/keyword')
+
 Route.get('health', async ({ response }) => {
   const report = await HealthCheck.getReport()
 
